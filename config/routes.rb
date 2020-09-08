7caa901ce971de1bf943e66db6fbfb7e41b80c9d
drsh1 <misiek.z.tarchomina@gmail.com>
2020-09-08 09:12:05 +0200

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
-Rails.application.routes.draw do
-  resources :cars
+#Rails.application.routes.draw do
+#  resources :cars
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#end
+
+# edited according to https://www.theodinproject.com/lessons/your-first-rails-application-ruby-on-rails
+Rails.application.routes.draw do
+  root 'cars#index'
+  resources :cars
 end
